038964752b75bb82cd7744090494a89f5012ed2e
Dreg <regueragarciadavid@gmail.com>
2023-07-19 16:44:02 +0200

    Update masm32.sh

--- masm32.sh
+++ masm32.sh
@@ -9,7 +9,7 @@ wine ml.exe -c -Zi -Fl -nologo /coff $1.asm
 echo ""
 echo "Linking..."
 echo ""
-wine link.exe /DEBUG /MAP /SUBSYSTEM:CONSOLE libs/Kernel32.Lib libs/User32.lib libs/Irvine32.lib libs/winmm.lib $1.obj
+wine link.exe /DEBUG /MAP /SUBSYSTEM:CONSOLE libs/Kernel32.Lib libs/User32.lib >
 echo "Executing..."
 echo ""
 cp -R ./res/* /tmp/
@@ -17,4 +17,5 @@ mv ./$1.exe /tmp/$1.exe
 wineconsole /tmp/$1.exe
 cp *.ro /tmp/
 rm $1.*
-} 2>&1  |  tee ./logmasm32.log
+}  2>&1 | tee logmasm32.log
+
