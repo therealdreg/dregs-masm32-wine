6f57985ac1a7aedf71c1c23f86e6405e6fcec13a
Dreg <regueragarciadavid@gmail.com>
2023-07-19 16:46:13 +0200

    Update masm32.sh

--- masm32.sh
+++ masm32.sh
@@ -5,11 +5,11 @@ echo ""
 cp $PP/$1.asm ./
 echo "Mounting..."
 echo ""
-wine ml.exe -c -Zi -Fl -nologo /coff $1.asm
+wine ml.exe -c -Zi -Fl -nologo $1.asm
 echo ""
 echo "Linking..."
 echo ""
-wine link.exe /DEBUG /MAP /SUBSYSTEM:CONSOLE libs/Kernel32.Lib libs/User32.lib >
+wine link.exe /DEBUG /MAP /SUBSYSTEM:CONSOLE libs/Kernel32.Lib libs/User32.lib libs/Irvine32.lib libs/winmm.lib $1.obj
 echo "Executing..."
 echo ""
 cp -R ./res/* /tmp/
